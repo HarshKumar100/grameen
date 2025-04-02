526ab15eeddcc3da19c1c0660ff525696707e137
HarshKumar100 <hk102004@gmail.com>
2025-04-03 04:00:22 +0530

    modified:   Backend/app.js 	modified:   Backend/server.js 	modified:   Backend/socket.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,12 @@
 const http = require('http');
 const app = require('./app');
 const { initializeSocket } = require('./socket');
-
 const port = process.env.PORT || 3000;
+
 const server = http.createServer(app);
 
 initializeSocket(server);
 
 server.listen(port, () => {
-    console.log(`🚀 Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
-
-// Debugging logs
-server.on("listening", () => console.log(`✅ Server is listening on port ${port}`));
-server.on("error", (err) => console.error("❌ Server error:", err));
